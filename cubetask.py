cd369e7da3b46c5b842ef333b6fd8a4214ec7025
grace-ataro <gracemaloba034@gmail.com>
2023-04-19 15:48:43 +0300

    python

---
+++ cubetask.py
@@ -0,0 +1,9 @@
+length=float(input("Enter length: "))
+def area(length):
+    return length * 6
+def volume(length):
+    return length * 3
+A=area(length)
+Vol=volume(length)
+print("The surface area of the cuboid is",A )
+print("The volume of the cuboid is", Vol)
--- task11.py
+++
@@ -1,3 +0,0 @@
-#Write a program that takes the date of birth of a person and the program outputs the age in terms of years,months,days TODAY.
-
-
